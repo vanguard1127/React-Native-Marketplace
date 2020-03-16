67a95c8c2e5467a29a38d6fd57ba8400db183f9b
vanguardk1127 <vanguard_K1127@outlook.com>
2020-03-16 14:08:18 +0800

    new commit

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export default {
+    webClientId: '522322123720-cotf2p172qn9ctj316m8h4ua70m0pic9.apps.googleusercontent.com',
+    iOSClientId: '522322123720-7mi8ckc0dt433pgah1d8v60c2gbtb4rl.apps.googleusercontent.com',
+    //REACT_APP_API_URL:'http://192.168.1.122:3030',
+    REACT_APP_API_URL:'http://35.236.50.166:3030',    
+    GOOGLE_MAP_API_KEY:'AIzaSyCeWSTokqJs2o1MPw7-S7KP16H57ODufHM',
+    STRIPE_PUB_KEY:'pk_test_VjrigsCUtX6uWYWa23jP5Tt400feU8fVEa'
+  };
